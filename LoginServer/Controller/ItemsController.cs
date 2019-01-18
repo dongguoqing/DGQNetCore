11a1e489a0ccb95b36f9cb21d48036de7f403dc6
1038467462 <1038467462@qq.com>
2019-01-18 18:30:11 +0800

    完善角色

---
+++ LoginServer/Controller/ItemsController.cs
@@ -0,0 +1,35 @@
+﻿using DGQ.Service.Contract;
+using Microsoft.AspNetCore.Mvc;
+using Model;
+using Model.ViewModel;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoginServer.Controller
+{
+    public class ItemsController : ControllerBase
+    {
+
+        private readonly IItemsService itemsService;
+        public ItemsController(IItemsService _itemsService)
+        {
+            this.itemsService = _itemsService;
+        }
+
+        [HttpGet(nameof(GetItemsList))]
+        [Route("api/Items/GetItemsList")]
+        public async Task<ActionResult> GetItemsList()
+        {
+            List<ItemsViewModel> list = await itemsService.GetItemsViewModel();
+            Dictionary<string, object> dic = new Dictionary<string, object>();
+            foreach (var item in list)
+            {
+                dic.Add(item.F_Id, item);
+            }
+            return Content(JsonConvert.SerializeObject(dic));
+        }
+    }
+}
